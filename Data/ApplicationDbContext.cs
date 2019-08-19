8a3ba457971d394ea6d71587cca3d65a4f0f4509
dana <danaw09@gmail.com>
2019-08-19 11:17:54 -0500

    initail commit

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -9,11 +9,12 @@ namespace GroceryStoreRewards.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-        public DbSet<Customer> Customer { get; set; }
-        public DbSet<CustomerPurchases> CustomerPurchases { get; set; }
+      
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+
         }
     }
 }
