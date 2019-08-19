a979b7c6234b844e8ac6018a68c3a108aab12bf3
dana <danaw09@gmail.com>
2019-08-19 10:20:31 -0500

    test

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using GroceryStoreRewards.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,6 +9,8 @@ namespace GroceryStoreRewards.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Customer> Customer { get; set; }
+        public DbSet<CustomerPurchases> CustomerPurchases { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
