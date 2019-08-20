22637f523fe3b1e2bb74d120c79220a1810bb7bf
EmilyKatie <emilygef10@gmail.com>
2019-08-20 15:49:21 -0500

    added correct database

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -9,35 +9,30 @@ namespace GroceryStoreRewards.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-
-<<<<<<< HEAD
-        public DbSet<Customer> Customer { get; set; }
-        public DbSet<ShoppingList> ShoppingLists { get; set; }
-       
-
-
-
-=======
->>>>>>> da6537fc5b46cfc1f9d8324d191149831ddf8724
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
-        {
+        { }
+        public DbSet<Customer> Customer { get; set; }
+        public DbSet<ShoppingList> ShoppingLists { get; set; }
 
-        }
-<<<<<<< HEAD
+        public DbSet<Recipes> Recipes { get; set; }
+    }
+}
+
+
+
+    
+    
         
 
           
-=======
-
-        public DbSet<Customer> Customer { get; set; }
-        public DbSet<Recipes> Recipes { get; set; }
 
 
-
->>>>>>> da6537fc5b46cfc1f9d8324d191149831ddf8724
-    }
+       
 
 
-}
+    
+
+
+
 
