baaf7456e632c9b150b5dcb5e22dc97b8093db0c
hamptoam <ameliabedelia94@gmail.com>
2019-08-22 14:20:04 -0500

    Fixed additional merge conflicts and removed a model that was replaced by another.

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -14,15 +14,7 @@ namespace GroceryStoreRewards.Data
             : base(options)
         { }
         public DbSet<Customer> Customer { get; set; }
-<<<<<<< HEAD
 
-=======
-
-<<<<<<< HEAD
-
-=======
->>>>>>> cb8bb93f1c2740859127868175941ddd92c50779
->>>>>>> f95223c0d82a40d8154da3543789a1803fabb5ae
         public DbSet<ShoppingList> ShoppingLists { get; set; }
 
         public DbSet<Recipes> Recipes { get; set; }
@@ -36,18 +28,7 @@ namespace GroceryStoreRewards.Data
             modelBuilder.Seed();
         }
 
-<<<<<<< HEAD
 
     
 }   }
-
-
-<<<<<<< HEAD
-=======
-=======
->>>>>>> cb8bb93f1c2740859127868175941ddd92c50779
-
->>>>>>> f95223c0d82a40d8154da3543789a1803fabb5ae
-    
-
 
