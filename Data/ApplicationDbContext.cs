32655360d573f7b8b562b101cdec100947b93b91
dana <danaw09@gmail.com>
2019-08-22 13:08:01 -0500

    added recipe controller and recipe dummy data for API requests later

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -14,38 +14,26 @@ namespace GroceryStoreRewards.Data
             : base(options)
         { }
         public DbSet<Customer> Customer { get; set; }
-<<<<<<< HEAD
-=======
 
->>>>>>> 158a5fa2b82d2e0edbadef26732d61bd62cb003e
+
         public DbSet<ShoppingList> ShoppingLists { get; set; }
 
         public DbSet<Recipes> Recipes { get; set; }
-<<<<<<< HEAD
+
 
 
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-       
-        modelBuilder.Entity<ShoppingList>().HasData(
-                  new ShoppingList
-                  {
-                      UserId = 1,
-                      FirstName = "mary",
-                      Ingredients = "Oats"
-                  }
-                  );
-            }
-
+            modelBuilder.Seed();
         }
-    }
 
-=======
-    }
-}
->>>>>>> 247495e1db156096fffc91567cc247580b3244ab
+
+    
+}   }
+
+
 
     
 
